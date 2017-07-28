d491094096efe36bff8bac4376373110e3b697c1
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-28 20:28:06 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  resources :meetups
+
+  resources :meetups do
+    resources comments
+  end
   root 'meetups#index'
   #root "welcome#index"
 end

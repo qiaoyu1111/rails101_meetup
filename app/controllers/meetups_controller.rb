faf5d98800cfaff79352364a5ca757427f9b75eb
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-28 13:11:50 +0800

    implement groups#create

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -9,6 +9,13 @@ class MeetupsController < ApplicationController
 
   def create
     @meetup = Meetup.new(meetup_params)
+    @meetup.save
     redirect_to meetups_path, notice: "Create success ."
+  end
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:title, :description)
   end
 end

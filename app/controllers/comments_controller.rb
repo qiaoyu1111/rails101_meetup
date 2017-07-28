6969d77a6d0bb31d6ba2698b0e91ee9417ce3b69
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-28 20:20:54 +0800

    add

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,12 @@ class CommentsController < ApplicationController
     @comment = @meetup.comments.create(params[:comment].permit(:body))
     redirect_to meetup_path(@meetup)
   end
+
+  def destroy
+    @meetup = Meetup.find(params[:meetup_id])
+    @comment = @meetup.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to meetup_path(@meetup)
+  end
 end

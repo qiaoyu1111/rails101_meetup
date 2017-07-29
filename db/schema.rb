d4a4f205179dcc0f85f235f9c5a348437e9e2228
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-29 09:34:17 +0800

    Add count cache for meetup comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729012557) do
+ActiveRecord::Schema.define(version: 20170729012939) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "message"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20170729012557) do
   create_table "meetups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
+    t.integer  "comments_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|

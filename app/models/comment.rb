882148e6a2b14e5d8e1d87444961be4f143529c6
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-12 23:03:17 +0800

    add

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :meetup, counter_cache: true
-  belongs_to :user
-end
--- app/models/meetup.rb
+++
@@ -1,5 +0,0 @@
-class Meetup < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true
-  has_many :comments
-end
